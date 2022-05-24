c04781fa19af4989a6ad719cabd88dd1c4ab675d
Breno Trindade <breno.trindade2011@gmail.com>
2022-05-23 23:24:05 -0300

    Creating the DBContext

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            Category[] categoriesToSeed = new Category[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                categoriesToSeed[i] = new Category 
+                { 
+                    categoryId = i,
+                    ThumbnailImagePath = "uploads/placeholder.jpg",
+                    Name = $"Category {i}",
+                    Description = $"A description of category {i}"
+                };
+            }
+
+            modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+        }
+    }
+}
