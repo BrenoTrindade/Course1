f027c6126a5980ad3151143080f3b3c6fbe999a4
Breno Trindade <breno.trindade2011@gmail.com>
2022-05-24 23:20:52 -0300

    Creating the Categories API

---
+++ Server/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Shared.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public CategoriesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Category> categories = await _dataContext.Categories.ToListAsync();
+
+            return Ok(categories);
+        }
+    }
+}
