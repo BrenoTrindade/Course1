99d9889474a7b1655c05e1a42805efcad09f379b
Breno Trindade <breno.trindade2011@gmail.com>
2022-05-24 23:48:39 -0300

    Consuming the categories API

---
+++ Client/Static/APIEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace Client.Static
+{
+    public static class APIEndpoints
+    {
+#if DEBUG
+        internal const string ServerBaseUrl = "https://localhost:5803";
+#else
+        internal const string ServerBaseUrl = "https://appname.azurewebsites.net";
+#endif
+
+        internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+    }
+}
