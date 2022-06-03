a1e00d6b60d747019180933a0649f2329d17296a
Breno Trindade <breno.trindade2011@gmail.com>
2022-06-02 22:56:56 -0300

    expanding the categories controller

--- Server/Controllers/CategoriesController.cs
+++ Server/Controllers/CategoriesController.cs
@@ -16,6 +16,7 @@ namespace Server.Controllers
         {
             _dataContext = dataContext;
         }
+        #region CRUD operations
 
         [HttpGet]
         public async Task<IActionResult> Get()
@@ -24,5 +25,64 @@ namespace Server.Controllers
 
             return Ok(categories);
         }
+
+        [HttpGet("withposts")]
+        public async Task<IActionResult> GetWithPosts()
+        {
+            List<Category> categories = await _dataContext.Categories
+                .Include(category => category.Posts)
+                .ToListAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            Category category = await GetCategoryByCategoryId(id, false);
+
+            return Ok(category);
+        }
+
+        [HttpGet("withpost/{id}")]
+        public async Task<ActionResult> GetWithPosts(int id)
+        {
+            Category category = await GetCategoryByCategoryId(id, true);
+
+            return Ok(category);
+        }
+        #endregion
+
+        #region Utility methods
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<bool> PersistChangesToDatabase()
+        {
+            int amountOfChanges = await _dataContext.SaveChangesAsync();
+
+            return amountOfChanges > 0;
+        }
+
+
+        [NonAction]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private async Task<Category> GetCategoryByCategoryId(int categoryId, bool withPosts)
+        {
+            Category categoryToGet = null;
+
+            if (withPosts == true)
+            {
+                categoryToGet = await _dataContext.Categories
+                    .Include(category => category.Posts)
+                    .FirstAsync(category => category.categoryId == categoryId);
+            }
+            else
+            {
+                categoryToGet = await _dataContext.Categories
+                    .FirstAsync(category => category.categoryId == categoryId);
+            }
+            return categoryToGet;
+        }
+        #endregion
     }
 }
